df730764fc81ca33034dd27bfe4b2fe15dd9fa61
Felipe-Agst <felipesp0607@gmail.com>
2024-02-25 19:24:17 -0300

    Desafio Curso em Video

---
+++ desafio_python02.py
@@ -0,0 +1,7 @@
+dia = int(input('Qual dia você nasceu?'))
+mes = (input('Qual mes você nasceu?'))
+ano = int(input('Qual ano você nasceu?'))
+
+
+
+print('Você nasceu no dia', dia, 'de', mes, 'de', ano, '. Correto?')
---
+++ desafio_python03.py
@@ -0,0 +1,7 @@
+numero01 = int(input('primeiro numero:'))
+
+numero02 = int(input('segunto numero:'))
+
+resultado = (numero01 + numero02)
+
+print('a soma de', numero01, 'mais', numero02, 'é igual a', resultado)
